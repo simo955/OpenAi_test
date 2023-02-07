8baf6e406139b327196a4e7106edb38ebd473cc7
simo955 <simone.patuelli@icloud.com>
2023-02-07 17:49:08 +0100

    Add support to Streamlit

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import os
 import openai
+import streamlit as st
 
+st.markdown("# OpenAi test 🎈")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-print("API KEY", openai.api_key)
+st.write("API KEY", openai.api_key)
+current_line_number = 1
 
-response = openai.Completion.create(
-  model="code-davinci-002",
-  prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSELECT",
-  temperature=0,
-  max_tokens=150,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["#", ";"]
-)
-
-print("response ", response)
+prompt = "### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### {}\nSELECT"
+input_query = st.text_input('Ask me anything', key=str(current_line_number))
+if input_query!='stop':
+    try:
+      response = openai.Completion.create(
+        model="code-davinci-002",
+        prompt=prompt.format(input_query),
+        temperature=0,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["#", ";"]
+      )
+      st.write("response ", response)
+    except Exception as exc:
+      st.write(f'En error occurred. {exc}')
